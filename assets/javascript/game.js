319738ad961728ce1c8e049479cbb71fd3e000e5
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-18 21:28:52 -0600

    added variables and character data

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+//Final Fantasy 7
+
+$(document).ready.(function () {
+
+    // Character object & stats
+
+  characters = {
+    name: ["Cloud", "Barrett", "Tifa", "Sephiroth"],
+    characterImages: ['', '*', '*', '*'],
+    characterHealth: [300, 200, 150, 275],
+    characterStr: [15, 10, 8, 13]
+  };
+
+    //create variables
+    var characterSelected = [];
+    var defenderSelected = [];
+    var selectedCharHealth = 0;
+    var selectedDefHealth = 0;
+    var charNotSelected = characters.name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
