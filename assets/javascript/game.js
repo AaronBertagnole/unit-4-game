880da9718f7f49f020593ca96703258842111764
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-22 12:44:49 -0600

    added the character object and stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,44 @@
 //Final Fantasy 7
 
-$(document).ready.(function () {
+$(document).ready(function () {
 
     // Character object & stats
 
   characters = {
-    name: ["Cloud", "Barrett", "Tifa", "Sephiroth"],
-    characterImages: ['', '*', '*', '*'],
-    characterHealth: [300, 200, 150, 275],
-    characterStr: [15, 10, 8, 13]
-  };
+    "Cloud Strife":{
+       name: "Cloud Strife",
+       healthPoints: 130,
+       attackPower: 8,
+       counterAttackPower: 15,
+       imageUrl: "./assets/images/0.png.png"
+    },
+    "Barrey Wallace":{
+      name: "Barrey Wallace",
+      healthPoints: 150,
+      attackPower: 6,
+      counterAttackPower: 13,
+      imageUrl: "./assets/images/1.png.png"
+    },
+    "Tifa Lockhart":{
+      name: "Tifa Lockhart ",
+      healthPoints: 100,
+      attackPower: 5,
+      counterAttackPower: 10,
+      imageUrl: "./assets/images/2.png"
+    },
+    "Sephiroth":{
+      name: "Sephiroth ",
+      healthPoints: 180,
+      attackPower: 7,
+      counterAttackPower: 25,
+      imageUrl: "./assets/images/4.png.png"
+    }  
+  }
+  console.log(characters);
 
-    //create variables
-    var characterSelected = [];
-    var defenderSelected = [];
-    var selectedCharHealth = 0;
-    var selectedDefHealth = 0;
-    var charNotSelected = characters.name;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+});
+ 
 
 
 
@@ -55,9 +53,3 @@ $(document).ready.(function () {
 
 
 
-
-
-
-
-
-})
